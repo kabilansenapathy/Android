59bfa0378322c915e1e9183f8ca5df46633d8c4c
Kabilan Senapathy <kabilansenapathy@live.in>
2016-06-29 11:54:37 +0530

    Created a listview and added sample data.

--- Basic_List_View/app/src/main/java/com/kabilansenapathy/basiclistview/MainActivity.java
+++ Basic_List_View/app/src/main/java/com/kabilansenapathy/basiclistview/MainActivity.java
@@ -3,11 +3,24 @@ package com.kabilansenapathy.basiclistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] SampleArray ={
+                "a",
+                "b",
+                "c"
+        };
+
+        List<String> listString = new ArrayList<String>(Arrays.asList(SampleArray)); //Get to know the differences between ArrayList and List
+
     }
 }
